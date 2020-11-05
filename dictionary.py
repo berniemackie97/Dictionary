ee7129af810acf651384fd13f24bfb0fd8468f55
berniemackie97 <44482337+berniemackie97@users.noreply.github.com>
2020-11-05 18:10:00 -0500

    Update
    Dictionary now checks for capitalization errors

--- dictionary.py
+++ dictionary.py
@@ -4,7 +4,11 @@ data = json.load(open("data.json"))
 
 
 def translate(w):
-    return data[w]
+    w = w.lower()
+    if w in data:
+        return data[w]
+    else:
+        return "Sorry I couldn't find your word"
 
 
 word = input("Enter word: ")

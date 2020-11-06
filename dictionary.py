48be8e92d6edd02ca51a31303a835afac0af9b0e
berniemackie97 <44482337+berniemackie97@users.noreply.github.com>
2020-11-05 20:22:13 -0500

    Add files via upload

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,6 @@
 import json
+from difflib import SequenceMatcher
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -7,8 +9,16 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        guess = input('Did you mean %s instead? Enter Y or N: ' % get_close_matches(w, data.keys())[0])
+        if guess == "Y" or guess == "y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif guess == "N" or guess == "n":
+            return "Sorry I couldn't find your word"
+        else:
+            return "I dont understand."
     else:
-        return "Sorry I couldn't find your word"
+        return "Sorry i couldn't find your word"
 
 
 word = input("Enter word: ")
